3deea134e3a27d9d597c372c62724c88a0c0821a
Jcastro <jcastroastelarra@gmail.com>
2022-09-06 16:19:19 +0200

    creada base de datos

--- streapp/models.py
+++ streapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+# Creacion de modelos para la base de datos.
+
+class Peliculas(models.Model):
+    nombre = models.CharField(max_length=128)
+    genero = models.CharField(max_length=128)
+    fecha = models.DateField() 
+    aptoPara = models.IntegerField()
+    def __str__(self):
+        return f"{self.nombre} - {self.genero}"
+
+class Series(models.Model):
+    nombre = models.CharField(max_length=128)
+    genero = models.CharField(max_length=128)
+    fecha = models.DateField()
+    aptoPara = models.IntegerField()
+    def __str__(self):
+        return f"{self.nombre} - {self.genero}"
+
+
+class Documentales(models.Model):
+    nombre = models.CharField(max_length=128)
+    genero = models.CharField(max_length=128)
+    fecha = models.DateField()
+    aptoPara = models.IntegerField()
+    def __str__(self):
+        return f"{self.nombre} - {self.genero}"
